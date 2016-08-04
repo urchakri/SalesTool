878e7e0bfd6e23cede0595507b2e0c785cf72e4a
chakradhar.duddikunt <chakradhar.duddikunt@CHAKRADHAR2.beghou.local>
2016-08-04 14:59:17 +0530

    Test2

--- SalesTool/src/SalesTool/Main.java
+++ SalesTool/src/SalesTool/Main.java
@@ -13,6 +13,7 @@ public class Main {
 		System.out.println("HELLO HAPPY SALES PEOPLE");
 		System.out.println("THIS APP SHOWS SALES DATA");
 		System.out.println("Test 1");
+		System.out.println("Test 2");
 		
 	}
 
